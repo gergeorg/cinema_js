9b2ccf30c57ad1633ca8eed90ff82f425d9ca79d
gergeorg <georgey.gerasimov@gmail.com>
2021-12-11 15:48:58 +0300

    add menu

---
+++ js/index.js
@@ -0,0 +1,10 @@
+import slideMenu from './menu.js'
+
+// slideMenu('.header__burger-btn', '.navigation', 'navigation_active', '.navigation__link', '.navigation__close')
+
+slideMenu({
+  openBtn: '.header__burger-btn',
+  menu: '.navigation',
+  classActiveMenu: 'navigation_active',
+  closeTrigger: '.navigation__link, .navigation__close',
+})
